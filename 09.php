dacb9c1ab18305adc757f4fdadc63c63287ce7ee
EduardoCollazoR <eduardo.collazo16@tectijuana.edu.mx>
2021-03-23 21:23:55 -0700

    poo mysqli

--- 09.php
+++ 09.php
@@ -1,4 +1,27 @@
 <?php include 'includes/header.php';
 
 
+//conectar a la base de datos Mysqli
+
+$db = new mysqli('localhost', 'root', '', 'bienes_raices');
+
+//creamos el query
+$query = "SELECT titulo, imagen from propiedades";
+
+// lo preparamos
+$stmt = $db->prepare($query);
+
+//lo ejecutamos
+$stmt->execute();
+
+//creamos la variable
+$stmt->bind_result($titulo, $imagen);
+
+//asignamos el resultado
+// imprimimos resultados
+while ($stmt->fetch()) :
+    var_dump($titulo);
+    var_dump($imagen);
+endwhile;
+
 include 'includes/footer.php';
