36b8ae28c9f7f68139f7be4a73856f8305c26604
EduardoCollazoR <eduardo.collazo16@tectijuana.edu.mx>
2021-03-23 20:49:27 -0700

    autoload y namespace

--- 08.php
+++ 08.php
@@ -4,15 +4,26 @@
 // incluir clases 
 
 
+use App\Clientes;
+use App\Detalles;
+
 
 function mi_autoload($clase)
 {
-    require __DIR__ . '/clases/' . $clase . '.php';
+
+    $partes = explode('\\', $clase);
+
+
+    require __DIR__ . '/clases/' . $partes[1] . '.php';
 }
 spl_autoload_register('mi_autoload');
 
 
+
+
+
 $detalles = new Detalles();
 $clientes = new Clientes();
+
 
 include 'includes/footer.php';
