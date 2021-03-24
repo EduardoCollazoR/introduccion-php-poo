cad254395c35f4b5b65b89b3ded0281f4ffc2ddd
EduardoCollazoR <eduardo.collazo16@tectijuana.edu.mx>
2021-03-23 20:24:00 -0700

    interfaces

--- 06.php
+++ 06.php
@@ -1,4 +1,28 @@
 <?php include 'includes/header.php';
+//interfaces
+// solo dice que funciones hay y que valores retorna
+interface TransporteInterfaz
+{
+    public function getInfo(): string;
+    public function getRuedas(): int;
+}
+class Transporte implements TransporteInterfaz
+{
+    public function __construct(protected int $ruedas, protected int $capacidad)
+    {
+    }
+
+    public function getInfo(): string
+    {
+        return "El transporte tiene " . $this->ruedas . " ruedas y una capacidad de " . $this->capacidad . " personas";
+    }
+
+    public function getRuedas(): int
+    {
+        return $this->ruedas;
+    }
+}
+
 
 
 include 'includes/footer.php';
