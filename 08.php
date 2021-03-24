caba5f4b863510389a15051206dd3beff378748d
EduardoCollazoR <eduardo.collazo16@tectijuana.edu.mx>
2021-03-23 20:39:14 -0700

    autoload clases

--- 08.php
+++ 08.php
@@ -1,4 +1,18 @@
 <?php include 'includes/header.php';
 
 
+// incluir clases 
+
+
+
+function mi_autoload($clase)
+{
+    require __DIR__ . '/clases/' . $clase . '.php';
+}
+spl_autoload_register('mi_autoload');
+
+
+$detalles = new Detalles();
+$clientes = new Clientes();
+
 include 'includes/footer.php';
